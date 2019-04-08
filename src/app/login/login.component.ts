52bcf00abb3d0ce3f0faa46c74d6c925429d5483
kshitizeSingh <kslucky12@gmail.com>
2019-04-08 11:50:56 +0530

    2 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+  loginForm = this.fb.group({
+    userName: ["", Validators.required],
+    password: ["", Validators.required],
+  })
 
   ngOnInit() {
+  }
+
+  login(){
+    this.loginForm.value
   }
 
 }
